96693971b3d7c9339d7af630e503be47b739b56e
Bread <213752396+NguyenVoManhHung@users.noreply.github.com>
2026-02-07 00:31:43 +0700

    more 2.5d stuff

--- Assets/Scripts/BillBoard.cs
+++ Assets/Scripts/BillBoard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class BillBoard : MonoBehaviour
 {
     private Camera mainCamera;
+    public bool isFlatMode = false;
     void Start()
     {
         mainCamera = Camera.main;
@@ -11,12 +12,19 @@ public class BillBoard : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.LookAt(mainCamera.transform);
-        //transform.Rotate(45, 0, 0); // rotation is off idk
-        // lock rotation on y axis
-        var rotation = transform.rotation.eulerAngles;
-        rotation.y = 0;
-        rotation.x = -rotation.x; // fix mirrored x axis
-        transform.rotation = Quaternion.Euler(rotation);
+        if (isFlatMode) // sprites copy camera rotation instead of looking at camera
+        {
+            transform.forward = mainCamera.transform.forward;
+        }
+        else {
+            transform.LookAt(mainCamera.transform);
+            var rotation = transform.rotation.eulerAngles;
+            // lock rotation on y axis
+            rotation.y = 0;
+            rotation.x = -rotation.x; // fix mirrored x axis
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        
+        
     }
 }

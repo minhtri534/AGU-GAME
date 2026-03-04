83e77f34e306ed15e3f2d5de01fb7744efd4d325
LeQuoc <quocthancongdai119@gmail.com>
2026-03-04 11:25:37 +0700

    minor change gun.cs

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -142,9 +142,9 @@ public class Gun : MonoBehaviour
             // and change the player influence to be a multiplier instead
             //float playerDamage = player.GetStats().Damage;
             //bullet.SetDamage(playerDamage);
-            bullet.damage = stats.GetDamage();
-            bullet.speed = stats.GetProjectileSpeed();
-            bullet.lifeTime = stats.GetProjectileLifeTime();
+            bullet.Damage = stats.GetDamage();
+            bullet.Speed = stats.GetProjectileSpeed();
+            bullet.LifeTime = stats.GetProjectileLifeTime();
             bullet.Size = stats.GetProjectileSize();
             // Add projectile components
             inventory.GetTypeModifierComponent()?.AddComponentsToProjectile(bullet);
